06fb96dc2ba6b8cc2db1d8770636e57dc902a1f0
macavich <jdealtrey@gmail.com>
2018-06-06 21:35:42 -0400

    PEP compliance

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-app = Flask(__name__)
+from flask import (Flask, render_template, request, redirect, jsonify,
+                   url_for, flash)
 
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
@@ -7,7 +7,8 @@ from sqlalchemy.orm.exc import NoResultFound
 from database_setup import Base, Sport, SportItem, User
 
 from flask import session as login_session
-import random, string
+import random
+import string
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
@@ -16,8 +17,9 @@ import json
 from flask import make_response
 import requests
 
+app = Flask(__name__)
 
-#Connect to Database and create database session
+# Connect to Database and create database session
 engine = create_engine('sqlite:///sportscatalog.db')
 Base.metadata.bind = engine
 
@@ -26,37 +28,41 @@ DBSession = sessionmaker(bind=engine)
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 
+
+# OAuth2 connection with Facebook
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
+    # confirm that this is the same user that visited /login/
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid State Parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
     # then exchange client token for the long-lived server-side token with GET
-    app_id = json.loads(open('fb_client_secrets.json','r').read())['web']['app_id']
-    app_secret = json.loads(open('fb_client_secrets.json','r').read())['web']['app_secret']
+    app_id = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = ('https://graph.facebook.com/oauth/access_token?grant_type=' +
-        'fb_exchange_token&client_id={}&client_secret={}'.format(app_id, app_secret) +
-        '&fb_exchange_token={}'.format(access_token))
+           'fb_exchange_token&client_id={}'.format(app_id) +
+           '&client_secret={}'.format(app_secret) +
+           '&fb_exchange_token={}'.format(access_token))
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     token = json.loads(result)['access_token']
-    #token = result.split('&')[0]
 
     # assuming the token works, we should be able to make api calls
-    url = 'https://graph.facebook.com/v2.8/me?access_token={}&fields=name,id,email'.format(token)
+    url = ('https://graph.facebook.com/v2.8/me?access_token=' +
+           '{}&fields=name,id,email'.format(token))
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    # print('url sent for API access: {}'.format(url))
-    # print('API JSON result: {}'.format(result))
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data['name']
     login_session['email'] = data['email']
     login_session['facebook_id'] = data['id']
-    # get photos
-    url = ('https://graph.facebook.com/{}'.format(login_session['facebook_id']) +
+    # get the users profile picture
+    url = ('https://graph.facebook.com/%s' % login_session['facebook_id'] +
            '/picture?type=square&redirect=0')
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
@@ -75,32 +81,36 @@ def fbconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += '" style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += '" style = "width: 300px; height: 300px;border-radius: 150px;'
+    output += '-webkit-border-radius: 150px;-moz-border-radius: 150px;">'
     flash("you are now logged in as %s" % login_session['username'])
     return output
 
+
+# OAuth2 connection with Google
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    # confirm that this is the same user that visited /login/
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     code = request.data
     try:
-        # Upgrade the authorization code into a credentials object
+        # change the authorization code into a flow object
         oauth_flow = flow_from_clientsecrets(
-            'client_secrets.json', scope = '', redirect_uri = 'postmessage')
+            'client_secrets.json', scope='', redirect_uri='postmessage')
         # exchanging authorization code for access token
         dCredentials = json.loads(oauth_flow.step2_exchange(code).to_json())
     except FlowExchangeError:
         response = make_response(json.dumps('Failed to upgrade the ' +
-            'authorization code.'), 401)
+                                            'authorization code.'), 401)
         response.headers['Content-Type'] = 'application.json'
         return response
     # check that the access token is valid
     access_token = dCredentials['access_token']
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' +
-        '{}'.format(access_token))
+           '{}'.format(access_token))
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
     # if there was an error in the access token info, abort
@@ -115,6 +125,7 @@ def gconnect():
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+    # verify that the google knows the correct client
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401
@@ -127,7 +138,7 @@ def gconnect():
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
         response = make_response(json.dumps('Current user is already' +
-            ' connected'), 200)
+                                            ' connected'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
     # get user info
@@ -157,11 +168,13 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += '" style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> ' #noqa
+    output += '" style = "width: 300px; height: 300px;border-radius: 150px;'
+    output += '-webkit-border-radius: 150px;-moz-border-radius: 150px;">'
     flash("you are now logged in as %s" % login_session['username'])
     return output
 
 
+# disconnect from either Google or Provider
 @app.route("/disconnect")
 def disconnect():
     # only disconnect a connected user
@@ -174,7 +187,8 @@ def disconnect():
             return response
         # Execute HTTP GET request to revoke current token
         access_token = dCredentials['access_token']
-        url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token #noqa
+        url = ('https://accounts.google.com/o/oauth2/revoke?' +
+               'token=%s' % access_token)
         headers = {'Content-type': 'application/x-www-form-urlencoded'}
         h = httplib2.Http()
         resp, content = h.request(url, 'POST', headers=headers)
@@ -187,6 +201,8 @@ def disconnect():
             flash("You have been successfully logged out!")
             return redirect(url_for('showSports'))
         else:
+            # for whatever reason, the given token was invalid
+            # we want to disconnect the user from our website to be safe
             if login_session.get('credentials') is not None:
                 del login_session['credentials']
             if login_session.get('gplus_id') is not None:
@@ -197,9 +213,8 @@ def disconnect():
                 del login_session['email']
             if login_session.get('picture') is not None:
                 del login_session['picture']
-            # for whatever reason, the given token was invalid
             response = make_response(json.dumps('Failed to revoke the token' +
-                ' for given user.'), 400)
+                                                ' for given user.'), 400)
             response.headers['Content-Type'] = 'application/json'
             return response
     elif login_session['provider'] == 'facebook':
@@ -215,37 +230,39 @@ def disconnect():
         flash("You have been successfully logged out!")
         return redirect(url_for('showSports'))
 
+
 @app.route('/login/')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase +
-        string.digits) for x in xrange(32))
+                    string.digits) for x in xrange(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
 
-#Show the homepage
+
+# Show the homepage
 @app.route('/')
 @app.route('/sports/')
 def showSports():
+    logged_in = False
     if 'username' in login_session:
-        print(login_session['username'])
         logged_in = True
-    else:
-        print("No one's logged in.")
-        logged_in = False
     session = DBSession()
     sports = session.query(Sport).order_by(asc(Sport.name)).all()
     dSports = {}
     items = session.query(SportItem).order_by(desc(SportItem.id)).all()
+    # handle only showing 10 items on the main page
     itemsToShow = []
     for index, item in enumerate(items):
         if index > 9:
             break
         else:
             itemsToShow.append(item)
+    # item to sport mapping
     for sport in sports:
         dSports[sport.id] = sport.name
     return render_template('sports.html', dSports=dSports,
-        itemsToShow=itemsToShow, logged_in=logged_in)
+                           itemsToShow=itemsToShow, logged_in=logged_in)
+
 
 # Show all items in a sport
 @app.route('/sport/<int:sport_id>/')
@@ -255,8 +272,8 @@ def showSportsItems(sport_id):
     if 'username' in login_session:
         logged_in = True
     session = DBSession()
-    sport = session.query(Sport).filter_by(id = sport_id).one()
-    items = session.query(SportItem).filter_by(sport_id = sport_id).all()
+    sport = session.query(Sport).filter_by(id=sport_id).one()
+    items = session.query(SportItem).filter_by(sport_id=sport_id).all()
     return render_template(
         'sportsItems.html', items=items, sport=sport, logged_in=logged_in)
 
@@ -276,12 +293,12 @@ def showSportItem(sport_id, item_id):
     if logged_in:
         return render_template('privateShowSportItem.html',
                                item=item, sport=sport, logged_in=logged_in)
-    return render_template(
-        'publicShowSportItem.html', item=item, sport=sport, logged_in=logged_in)
+    return render_template('publicShowSportItem.html', item=item,
+                           sport=sport, logged_in=logged_in)
 
 
-#Create a new sport item
-@app.route('/sports/new/',methods=['GET','POST'])
+# Create a new sport item
+@app.route('/sports/new/', methods=['GET', 'POST'])
 def newSportItem():
     if 'username' not in login_session:
         flash("You must be logged in to create a new Item!")
@@ -292,15 +309,15 @@ def newSportItem():
         if request.form['sport']:
             try:
                 sport_id = session.query(Sport).filter_by(
-                    name = request.form['sport']).one().id
+                    name=request.form['sport']).one().id
             except NoResultFound:
                 flash("You didn't enter a valid sport, please choose from " +
                       "one of the below sports")
                 return redirect(url_for('showSports'))
-            newItem = SportItem(name = request.form['name'],
-                                description = request.form['description'],
-                                sport_id = sport_id,
-                                user_id = login_session['user_id'])
+            newItem = SportItem(name=request.form['name'],
+                                description=request.form['description'],
+                                sport_id=sport_id,
+                                user_id=login_session['user_id'])
             session.add(newItem)
             session.commit()
             flash('New Sport %s Item Successfully Created' % (newItem.name))
@@ -318,8 +335,8 @@ def newSportItem():
 
 
 # #Edit a sport item
-@app.route(
-    '/sports/<int:sport_id>/items/<int:item_id>/edit/', methods=['GET','POST'])
+@app.route('/sports/<int:sport_id>/items/<int:item_id>/edit/',
+           methods=['GET', 'POST'])
 def editSportItem(sport_id, item_id):
     if 'username' not in login_session:
         flash("You must be logged in to edit items!")
@@ -327,12 +344,12 @@ def editSportItem(sport_id, item_id):
     logged_in = True
     session = DBSession()
     try:
-        itemToEdit = session.query(SportItem).filter_by(id = item_id).one()
+        itemToEdit = session.query(SportItem).filter_by(id=item_id).one()
     except NoResultFound:
         flash("You were looking for an item that doesn't exist!")
         return redirect(url_for('showSports'))
     try:
-        sport = session.query(Sport).filter_by(id = sport_id).one()
+        sport = session.query(Sport).filter_by(id=sport_id).one()
     except NoResultFound:
         flash("You were looking for a sport that doesn't exist!")
         return redirect(url_for('showSports'))
@@ -346,7 +363,7 @@ def editSportItem(sport_id, item_id):
             itemToEdit.description = request.form['description']
         if request.form['sport']:
             newSport = session.query(Sport).filter_by(
-                name = request.form['sport']).one()
+                name=request.form['sport']).one()
             itemToEdit.sport_id = newSport.id
         session.add(itemToEdit)
         session.commit()
@@ -358,9 +375,9 @@ def editSportItem(sport_id, item_id):
                                sport=sport, sports=sports, logged_in=logged_in)
 
 
-#Delete a sport item
+# Delete a sport item
 @app.route('/sports/<int:sport_id>/items/<int:item_id>/delete/',
-           methods = ['GET','POST'])
+           methods=['GET', 'POST'])
 def deleteSportItem(sport_id, item_id):
     if 'username' not in login_session:
         flash("You must be logged in to delete items!")
@@ -368,12 +385,12 @@ def deleteSportItem(sport_id, item_id):
     logged_in = True
     session = DBSession()
     try:
-        itemToDelete = session.query(SportItem).filter_by(id = item_id).one()
+        itemToDelete = session.query(SportItem).filter_by(id=item_id).one()
     except NoResultFound:
         flash("You were looking for an item that doesn't exist!")
         return redirect(url_for('showSports'))
     try:
-        sport = session.query(Sport).filter_by(id = sport_id).one()
+        sport = session.query(Sport).filter_by(id=sport_id).one()
     except NoResultFound:
         flash("You were looking for a sport that doesn't exist!")
         return redirect(url_for('showSports'))
@@ -386,18 +403,45 @@ def deleteSportItem(sport_id, item_id):
         flash('Sport Item {} Successfully Deleted'.format(itemToDelete.name))
         return redirect(url_for('showSports'))
     else:
-        return render_template('deleteSportItem.html', item = itemToDelete,
+        return render_template('deleteSportItem.html', item=itemToDelete,
                                sport=sport, logged_in=logged_in)
 
 
+# JSON endpoint for all categories data
+@app.route('/sports/JSON/')
+def showSportsJSON():
+    session = DBSession()
+    sports = session.query(Sport).all()
+    return jsonify(categories=[sport.serialize for sport in sports])
+
+
+# JSON endpoint for all items in a category
+@app.route('/sports/<int:sport_id>/JSON/')
+def showSingleSportJSON(sport_id):
+    session = DBSession()
+    sport = session.query(Sport).filter_by(id=sport_id).one()
+    items = session.query(SportItem).filter_by(sport_id=sport_id).all()
+    return jsonify(
+        category=sport.serialize, items=[i.serialize for i in items])
+
+
+# JSON endpoint for a single item
+@app.route('/sports/<int:sport_id>/items/<int:item_id>/JSON/')
+def showSportItemJSON(sport_id, item_id):
+    session = DBSession()
+    item = session.query(SportItem).filter_by(id=item_id).one()
+    return jsonify(item.serialize)
+
+
+# handling getting a users data
 def getUserID(email):
     session = DBSession()
     try:
         try:
-            user = session.query(User).filter_by(email = email).one()
+            user = session.query(User).filter_by(email=email).one()
             return user.id
         except:
-            user = session.query(User).filter_by(email = email).all()
+            user = session.query(User).filter_by(email=email).all()
             return user[0].id
     except:
         return None
@@ -405,13 +449,10 @@ def getUserID(email):
 
 def createUser(login_session):
     session = DBSession()
-    newUser = User(name = login_session['username'],
-                   picture = login_session['picture'],
-                   email = login_session['email'])
-    # make sure we don't already have this user's email
+    # make sure we don't already have this user in our DB
     try:
         user = session.query(User).filter_by(
-            email = login_session['email']).one()
+            email=login_session['email']).one()
         # looks like this user's email already exists in our DB
         # lets update the information with the current login_session
         user.name = login_session['username']
@@ -420,13 +461,18 @@ def createUser(login_session):
         session.add(user)
         session.commit()
         return user.id
-    except:
+    except NoResultFound:
         pass
+    newUser = User(name=login_session['username'],
+                   picture=login_session['picture'],
+                   email=login_session['email'])
     session.add(newUser)
     session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8000)
+    app.run(host='0.0.0.0', port=8000)
